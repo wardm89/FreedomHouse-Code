33934929a3e039fb5d4268cc3962754150123f75
Matthew Ward <wardm89@yahoo.com>
2023-01-25 12:21:36 -0500

    Updated Care Case description

--- src/ministryPlatformApi/careCase.py
+++ src/ministryPlatformApi/careCase.py
@@ -1,7 +1,7 @@
 class CareCase():
     def __init__(self, pastoral_care_request):
         self.title = CareCase.get_title(pastoral_care_request)
-        self.description = pastoral_care_request['answers'][4]['Response']
+        self.description = CareCase.get_description(pastoral_care_request)
         self.household_id = pastoral_care_request['contact'][0]['Household_ID'] # "Household Record" 2 is Inactive | 206 Laurel Crest Dr | Kannapolis
         self.contact_id = pastoral_care_request['Contact_ID'] # "People Record" 2 Is the Default Contact ID 
         self.start_date = pastoral_care_request['Response_Date']
@@ -30,3 +30,18 @@ class CareCase():
     @staticmethod
     def get_title(pastoral_care_request):
         return pastoral_care_request['answers'][0]['Response'] + ' ' + pastoral_care_request['answers'][3]['Response']
+
+    @staticmethod
+    def get_description(pastoral_care_request):
+        desc = f"Recipient: {pastoral_care_request['answers'][0]['Response']}"
+        desc = desc + "\n" + f"Circumstance: {pastoral_care_request['answers'][3]['Response']}"
+        desc = desc + "\n"
+        desc = desc + "\n" + f"Campus: {pastoral_care_request['answers'][1]['Response']}"
+        desc = desc + "\n" + f"Care recipient is a: {pastoral_care_request['answers'][2]['Response']}"
+        desc = desc + "\n"
+        desc = desc + "\n" + f"Please describe the need. {pastoral_care_request['answers'][4]['Response']}"
+        desc = desc + "\n" + f"When and how did you become aware of the need? {pastoral_care_request['answers'][5]['Response']}"
+        desc = desc + "\n" + f"What has been done? {pastoral_care_request['answers'][6]['Response']}"
+        desc = desc + "\n"
+        desc = desc + "\n" + f"Please share Contact Number and any other information to assist with follow up care. {pastoral_care_request['answers'][7]['Response']}"
+        return desc
